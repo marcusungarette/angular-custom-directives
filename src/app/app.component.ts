e6a20542163cc32733d9daefa35b460be2e514f4
marcusungarette <marcusungarette@gmail.com>
2021-03-30 11:32:58 -0300

    Addedng-container and checkWindoIndex method to handle with increasing pages bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,5 +28,29 @@ export class AppComponent {
       url:
         'https://images.unsplash.com/photo-1511497584788-876760111969?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80',
     },
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    },
+    {
+      title: 'At the Snow Montain',
+      url:
+        'https://images.unsplash.com/photo-1531436107035-40b2e85b7a1b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    },
+    {
+      title: 'At the City',
+      url:
+        'https://images.unsplash.com/photo-1449824913935-59a10b8d2000?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    },
+    {
+      title: 'At the Forest',
+      url:
+        'https://images.unsplash.com/photo-1511497584788-876760111969?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80',
+    },
   ];
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
